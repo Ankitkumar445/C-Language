bee8be6f8f0a590641a7cc3cab9d09ba6fdb88c2
Ankit kumar <169060244+Ankitkumar445@users.noreply.github.com>
2024-07-30 06:12:22 +0530

    Add files via upload

---
+++ chapter2 (Instructions and operator)/ARI.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a =6;
+    int b=3;
+    int c=a+b;
+    printf("The value of a is %d and the value of b is %d and sum is %d\n",a,b,c);
+    printf("the remainder when a is div ivde by b is: %d\n",a%b);
+    return 0;
+}
---
+++ chapter2 (Instructions and operator)/Q3.c
@@ -0,0 +1,8 @@
+//Write a program o check whether the number is divisible by 97 or not?
+#include <stdio.h>
+
+int main(){
+    int a =9681968;
+    printf("The value of a%97 is %d",a%97);
+    return 0;
+}
